fc71859b9970295a08983321774da419b023a7cb
Tao Bian <biantao0519@hotmail.com>
2023-01-19 10:39:32 +1100

    add timer

--- index.js
+++ index.js
@@ -6,7 +6,24 @@ const homeBtn3 = document.getElementById('home-btn3')
 const guestBtn1 = document.getElementById('guest-btn1')
 const guestBtn2 = document.getElementById('guest-btn2')
 const guestBtn3 = document.getElementById('guest-btn3')
-const restartBtn = document.getElementById('restart')
+const startBtn = document.getElementById('start')
+const resetBtn = document.getElementById('reset')
+const timeContainer = document.getElementById('timer-container')
+
+resetBtn.disabled = true
+homeBtn1.disabled = true
+homeBtn2.disabled = true
+homeBtn3.disabled = true
+guestBtn1.disabled = true
+guestBtn2.disabled = true
+guestBtn3.disabled = true
+
+
+const startingMinutes = 60
+let time = startingMinutes * 60
+const timerEl = document.getElementById('timer')
+
+
 
 function homeAdd1() {
     home.textContent = Number(home.textContent) + 1
@@ -32,10 +49,48 @@ function guestAdd3() {
     guest.textContent = Number(guest.textContent) + 3
 }
 
-function restart() {
+
+function timer() {
+    const minutes = Math.floor(time / 60)
+    let seconds = time % 60
+    // if seconds < 10, add '0' 
+    seconds = seconds < 10 ? '0' + seconds : seconds
+    timerEl.textContent = `${minutes} : ${seconds}`
+    time--
+}
+
+function reset() {
+    console.log('Game Reset!')
+    resetBtn.disabled = true
+    startBtn.disabled = false
+    homeBtn1.disabled = true
+    homeBtn2.disabled = true
+    homeBtn3.disabled = true
+    guestBtn1.disabled = true
+    guestBtn2.disabled = true
+    guestBtn3.disabled = true
+    clearInterval(countTime)
     home.textContent = 0
     guest.textContent = 0
+    timerEl.textContent = "60 : 00"
+    timeContainer.appendChild(`<p>Game reset!<p>`)
 }
+
+function newGame() {
+    console.log('Game Start!')
+    startBtn.disabled = true
+    resetBtn.disabled = false
+    homeBtn1.disabled = false
+    homeBtn2.disabled = false
+    homeBtn3.disabled = false
+    guestBtn1.disabled = false
+    guestBtn2.disabled = false
+    guestBtn3.disabled = false
+    countTime = setInterval(timer, 1000)
+    timeContainer.appendChild(`<p>Game reset!<p>`)
+}
+
+
 
 
 homeBtn1.addEventListener('click', homeAdd1)
@@ -44,4 +99,5 @@ homeBtn3.addEventListener('click', homeAdd3)
 guestBtn1.addEventListener('click', guestAdd1)
 guestBtn2.addEventListener('click', guestAdd2)
 guestBtn3.addEventListener('click', guestAdd3)
-restartBtn.addEventListener('click', restart)
+startBtn.addEventListener('click', newGame)
+resetBtn.addEventListener('click', reset)
